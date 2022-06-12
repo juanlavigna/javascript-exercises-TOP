77211cd921ffbbd46b1e5b51a275facddf51e5bb
Juan Lavigna <juanlavigna@gmail.com>
2022-06-12 16:37:20 -0300

    Solve exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
 const removeFromArray = function() {
-
+    let array = arguments[0]
+    for (i = 1; i < arguments.length; i++ ){
+        let index = array.indexOf(arguments[i])
+        if(index === -1){
+            continue
+        }else{
+            array.splice(index, 1)
+        }
+    }
+    return array
 };
 
 // Do not edit below this line
