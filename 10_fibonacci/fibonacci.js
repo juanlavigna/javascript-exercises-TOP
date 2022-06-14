49582d71d738b38a77dce00ca7d81cd9d1fe4f72
Juan Lavigna <juanlavigna@gmail.com>
2022-06-14 14:46:14 -0300

    Solves exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    if(number<0) return "OOPS";
+    
+    let previous = 0
+    let previous2 = 1
+    for(let i = 1; i < number; i++){
+        let temp = previous2
+        previous2 += previous   
+        previous = temp
 
+    }
+    return previous2
 };
-
+// 1, 1, 2, 3, 5, 8, 13,  etc.
 // Do not edit below this line
 module.exports = fibonacci;
