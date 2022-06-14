54076f04dcec5376c9d805a47c2eb40203e019ca
Juan Lavigna <juanlavigna@gmail.com>
2022-06-14 14:29:31 -0300

    Solves exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    let newWord = word.toLowerCase().replace(/[^a-z]/g, "");
+    let reverseWord = newWord.split("").reverse().join("")
+    if (newWord === reverseWord){
+        return true
+    }else{
+        return false
+    }
 };
-
+palindromes("Menem")
 // Do not edit below this line
 module.exports = palindromes;
